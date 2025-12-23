8f0a996d2db5ba37393f301e9225e3c2a0b685ad
cloudakshay7 <cloudakshay7@gmail.com>
2025-12-23 14:21:49 +0530

    variable file uploaded

---
+++ compute/variable.tf
@@ -0,0 +1,22 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "The GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "instance_name" {
+  description = "Name of the compute instance"
+  type        = string
+  default     = "terraform-vm"
+}
